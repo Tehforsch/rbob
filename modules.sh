9375e5cb1c04704eae308ca8f4d133c768d7990b
tehforsch <Tonipeter92@googlemail.com>
2021-02-03 15:51:53 +0100

    update to properly compile hdf5 on cluster

---
+++ modules.sh
@@ -0,0 +1,8 @@
+module unload lib/hdf5/1.8-intel-16.0 
+module unload devel/python_intel/3.6
+module unload mpi/impi/5.1.3-intel-16.0
+module unload numlib/gsl/2.2.1-intel-16.0
+module unload numlib/fftw/3.3.5-impi-5.1.3-intel-16.0
+module unload compiler/intel/16.0
+module load devel/cmake/3.17.3
+module load compiler/gnu/9.1
