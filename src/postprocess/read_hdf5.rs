c14c7bebe16f8b12d9aa5c438a162293a6fac205
tehforsch <Tonipeter92@googlemail.com>
2021-05-30 12:32:37 +0200

    fix for merged attribute PR, use newer version of ndarray and ndarray-stats

--- src/postprocess/read_hdf5.rs
+++ src/postprocess/read_hdf5.rs
@@ -4,7 +4,7 @@ pub fn get_header_attribute<Q>(file: &hdf5::File, name: &str, unit: Q) -> Result
 where
     Q: Clone + std::ops::Mul<f64, Output = Q>,
 {
-    let attr = file.group("Header")?.attribute(name)?;
+    let attr = file.group("Header")?.attr(name)?;
     let value: Vec<f64> = attr.read_raw()?;
     assert_eq!(value.len(), 1);
     Ok(unit * value[0])
