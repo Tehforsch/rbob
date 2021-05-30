4f76a73cca4649e43d97c4c7d9049b4fcd01dad9
tehforsch <Tonipeter92@googlemail.com>
2021-05-30 13:03:29 +0200

    add script to compile new version on bwfor

--- buildBwfor.sh
+++ buildBwfor.sh
@@ -5,4 +5,6 @@ module unload numlib/gsl/2.2.1-intel-16.0
 module unload numlib/fftw/3.3.5-impi-5.1.3-intel-16.0
 module unload compiler/intel/16.0
 module load devel/cmake/3.17.3
-module load compiler/gnu/9.1
+module load lib/hdf5/1.10-gnu-7.1
+export HDF5_VERSION="1.10.1"
+export HDF5_DIR=/opt/bwhpc/common/lib/hdf5/1.10.1-gnu-7.1/
