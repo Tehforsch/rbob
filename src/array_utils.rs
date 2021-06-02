b74fc18727f400311b44fbe1861a4534a94d0cc1
tehforsch <Tonipeter92@googlemail.com>
2021-06-02 10:16:05 +0200

    move slices to center, fix

--- src/array_utils.rs
+++ src/array_utils.rs
@@ -11,12 +11,12 @@ pub type FArray2 = Array2<Float>;
 pub type FArray3 = Array3<Float>;
 pub type FArray4 = Array4<Float>;
 
-pub fn convert_heatmap_to_gnuplot_format(heatmap: FArray2, length_factor: f64) -> FArray2 {
+pub fn convert_heatmap_to_gnuplot_format(heatmap: FArray2, center: FArray1, length_factor: f64) -> FArray2 {
     let shape = heatmap.shape();
     let mut result = FArray2::zeros((shape[0] * shape[1], 3));
     for ((i0, i1), v) in heatmap.indexed_iter() {
-        result[[i0 * shape[0] + i1, 0]] = i0 as f64 / NX_SLICE as f64 * length_factor;
-        result[[i0 * shape[0] + i1, 1]] = i1 as f64 / NY_SLICE as f64 * length_factor;
+        result[[i0 * shape[0] + i1, 0]] = ((i0 as f64 / NX_SLICE as f64) - center[[0]]) * length_factor;
+        result[[i0 * shape[0] + i1, 1]] = ((i1 as f64 / NY_SLICE as f64) - center[[1]]) * length_factor;
         result[[i0 * shape[0] + i1, 2]] = *v as f64;
     }
     result
