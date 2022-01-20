80e368dcd43432f1cc1c7a67721df757e8d3b561
tehforsch <mail@tonipeter.de>
2022-01-20 14:53:19 +0100

    fix build script

--- buildSupermuc.sh
+++ buildSupermuc.sh
@@ -1,2 +1,4 @@
 cross build --release --target x86_64-unknown-linux-gnu
-cp target/x86_64-unknown-linux-gnu/release/bob $supermucProjects/bob/
+if [[ $? == 0 ]]; then
+    cp target/x86_64-unknown-linux-gnu/release/bob $supermucProjects/bob/
+fi
