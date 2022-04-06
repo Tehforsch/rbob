2828cc6cfdd9c7fac4474398bbfda8af7305b982
tehforsch <mail@tonipeter.de>
2022-04-06 16:33:05 +0200

    show sims for each sim set folder, properly get sims folders

--- src/gui/gui_sim_set.rs
+++ src/gui/gui_sim_set.rs
@@ -1,3 +1,5 @@
+use bob::sim_params::SimParams;
+use bob::sim_set::SimSet;
 use camino::Utf8PathBuf;
 
 #[derive(PartialEq, Eq)]
@@ -8,5 +10,12 @@ pub struct GuiSimSet {
 impl GuiSimSet {
     pub fn name(&self) -> &str {
         self.path.file_name().unwrap()
+    }
+
+    pub fn get_sims(&self) -> impl Iterator<Item = SimParams> {
+        SimSet::from_output_folder(&self.path)
+            .unwrap()
+            .into_iter()
+            .map(|(_, s)| s)
     }
 }
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,7 +1,12 @@
+use std::iter;
+use std::iter::once;
+
+use bob::sim_params::SimParams;
 use bob::util::get_folders;
 use camino::Utf8Path;
 use eframe::egui::Button;
 use eframe::egui::TextStyle;
+use eframe::egui::Ui;
 use eframe::egui::{self};
 use eframe::epi;
 
@@ -13,56 +18,109 @@ mod gui_sim_set;
 
 pub struct BobGui {
     sim_sets: Vec<GuiSimSet>,
-    selected: usize,
+    selected: Vec<usize>,
+    sims: Vec<SimParams>,
 }
 
 fn discover_sims(path: &Utf8Path) -> Vec<GuiSimSet> {
-    get_folders(path)
-        .unwrap()
-        .into_iter()
-        .map(|path| GuiSimSet { path })
-        .collect()
+    discover_sims_iter(path).collect()
+}
+
+fn discover_sims_iter(path: &Utf8Path) -> Box<dyn Iterator<Item = GuiSimSet>> {
+    let folders = get_folders(path).unwrap();
+    Box::new(
+        folders
+            .into_iter()
+            .flat_map(move |path| {
+                if get_folders(&path)
+                    .unwrap()
+                    .iter()
+                    .find(|path| path.file_name() == Some("0"))
+                    .is_some()
+                {
+                    Box::new(iter::once(GuiSimSet { path: path.into() }))
+                } else {
+                    discover_sims_iter(&path)
+                }
+            })
+            .into_iter(),
+    )
 }
 
 impl BobGui {
     pub fn new(path: &Utf8Path) -> Self {
         Self {
             sim_sets: discover_sims(path),
-            selected: 0,
+            selected: vec![],
+            sims: vec![],
         }
     }
 
-    fn find_index(&self, sim: &GuiSimSet) -> usize {
-        self.sim_sets
-            .iter()
-            .enumerate()
-            .find(|(i, s)| *s == sim)
-            .map(|(i, _)| i)
-            .unwrap()
+    fn get_selected(&self) -> impl Iterator<Item = &GuiSimSet> {
+        self.selected.iter().map(|index| &self.sim_sets[*index])
     }
 
-    fn add_side_panel(&mut self, ctx: &egui::CtxRef) {
-        egui::SidePanel::left("side_bar")
+    fn add_or_remove_from_selection(&mut self, sim_index: usize) {
+        let index = self
+            .selected
+            .iter()
+            .enumerate()
+            .find(|(_, sim_i)| *sim_i == &sim_index)
+            .map(|(index, _)| index);
+        if let Some(index) = index {
+            self.selected.remove(index);
+        } else {
+            self.selected.push(sim_index);
+        }
+    }
+
+    fn select(&mut self, sim_index: usize) {
+        self.add_or_remove_from_selection(sim_index);
+        self.sims = self
+            .get_selected()
+            .flat_map(|sim_set| sim_set.get_sims())
+            .collect();
+    }
+
+    fn show_sim_set_buttons_and_handle_selection(&mut self, ui: &mut Ui) {
+        let mut selected = None;
+        for (i, sim) in self.sim_sets.iter().enumerate() {
+            let mut button = Button::new(sim.name()).text_style(TextStyle::Heading);
+            if self.selected.contains(&i) {
+                button = button.fill(SELECTED_COLOR);
+            }
+            let response = ui.add(button);
+            if response.clicked() {
+                selected = Some(i);
+            }
+        }
+        if let Some(selected) = selected {
+            self.select(selected);
+        }
+    }
+
+    fn add_sim_set_selection_panel(&mut self, ctx: &egui::CtxRef) {
+        egui::SidePanel::left("sim_set_bar")
+            .resizable(false)
+            .min_width(config::MIN_SIDE_BAR_WIDTH)
+            .show(ctx, |mut ui| {
+                self.show_sim_set_buttons_and_handle_selection(&mut ui);
+            });
+    }
+
+    fn add_sim_selection_panel(&mut self, ctx: &egui::CtxRef) {
+        egui::SidePanel::left("sim_bar")
             .resizable(false)
             .min_width(config::MIN_SIDE_BAR_WIDTH)
             .show(ctx, |ui| {
-                for (i, sim) in self.sim_sets.iter().enumerate() {
-                    let mut button = Button::new(sim.name()).text_style(TextStyle::Heading);
-                    if i == self.selected {
-                        button = button.fill(SELECTED_COLOR);
-                    }
-                    let response = ui.add(button);
-                    if response.clicked() {
-                        self.selected = self.find_index(sim);
-                    }
+                for sim in self.sims.iter() {
+                    ui.label(sim.get_name());
                 }
             });
     }
 
     fn add_central_panel(&mut self, ctx: &egui::CtxRef) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("ho");
-        });
+        egui::CentralPanel::default().show(ctx, |ui| {});
     }
 }
 
@@ -72,7 +130,8 @@ impl epi::App for BobGui {
     }
 
     fn update(&mut self, ctx: &egui::CtxRef, _: &epi::Frame) {
-        self.add_side_panel(ctx);
+        self.add_sim_set_selection_panel(ctx);
+        self.add_sim_selection_panel(ctx);
         self.add_central_panel(ctx);
     }
 }
