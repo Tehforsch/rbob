5a37c226cfabfb97e926f30711570a075f6f522a
tehforsch <mail@tonipeter.de>
2023-05-15 18:18:17 +0200

    use remake instead of make because make is a buggy, forkbombing piece of shit

--- src/make.rs
+++ src/make.rs
@@ -71,7 +71,7 @@ fn write_systype_file(systype: &Option<Systype>) -> Result<()> {
 fn build_arepo(verbose: bool) -> Result<()> {
     delete_arepoconfig_header_file_if_present(&AREPO_PATH)?;
     let out = get_shell_command_output(
-        "make",
+        "remake",
         &[
             &"-j",
             &config::DEFAULT_NUM_CORES_TO_COMPILE.to_string().as_ref(),
